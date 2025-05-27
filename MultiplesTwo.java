a6007d71b743db7f8da6261fbe34c8c494907e4e
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2025-05-28 01:31:34 +0600

    Update and rename Triangle.java to MultiplesTwo.java

---
+++ MultiplesTwo.java
@@ -0,0 +1,13 @@
+package chapter_4
+
+public class MultiplesTwo {
+    public static void main(String[] args) {
+        int number = 2;
+
+        while (true) {
+            System.out.println(number);
+            number += 2;
+        }
+    }
+}
+
