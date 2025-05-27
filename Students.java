e4c470c8eaaccd2fa053d7c07c76a3bf2c89035d
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2025-05-28 01:04:25 +0600

    Create Students.java

---
+++ Students.java
@@ -0,0 +1,27 @@
+package chapter_3;
+
+public class Student {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public Student(String name) {
+        this.name= name;
+    }
+
+    public static void main(String[] args) {
+
+        Student student= new Student("Rohima");
+        
+        System.out.println(student.getName());
+
+
+
+    }
+}
