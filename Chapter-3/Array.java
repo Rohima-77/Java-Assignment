7fd26ea2e5b925e61eaf85e32169b33f0c0df3f7
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2025-06-02 23:14:45 +0600

    Create Array.java

---
+++ Chapter-3/Array.java
@@ -0,0 +1,12 @@
+chapter_3
+public class Array
+ {
+ public static void main( String[] args )
+   {
+      int[] array; 
+      array = new int[ 10 ];
+     System.out.printf( "%s%8s\n", "Index", "Value" );
+     for ( int counter = 0; counter < array.length; counter++ )
+       System.out.printf( "%5d%8d\n", counter, array[ counter ] );
+   }
+ }
