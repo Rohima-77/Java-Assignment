6fbb6cdde8c579a3eb468a324a2c4fa777ff3c24
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2025-06-03 15:50:10 +0600

    Create MultiplesTwo.java

---
+++ Chapter-4/MultiplesTwo.java
@@ -0,0 +1,12 @@
+package chapter_4
+
+public class MultiplesTwo {
+    public static void main(String[] args) {
+        int number = 2;
+
+        while (true) {
+            System.out.println(number);
+            number += 2;
+        }
+    }
+}
